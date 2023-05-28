3b20029628c386412eeb7e582f00f2c4f25b4914
Darya Filimonova <91047620+DaryaFilimonova7@users.noreply.github.com>
2023-05-28 20:51:38 +0300

    Update ex1_C.py

--- ex1_C.py
+++ ex1_C.py
@@ -1,18 +1,22 @@
+from collections import Counter
+
 ans = [int(n1) for n1 in str(input())]
 guess = [int(n2) for n2 in str(input())]
 
 bull = 0
 cow = 0
+counts = Counter(ans)
 
-for i in range(len(ans)):
-        if guess[i] == ans[i]:
-            bull += 1
-        if ans[i] in guess:
-            cow += 1
-cow = cow - bull
+for i,j in zip(ans, guess):
+    if i==j:
+        bull += 1
+        if counts[i]:
+            counts[i] -= 1
+        else:
+            cow -= 1
+    elif counts[j]:
+        cow += 1
+        counts[j] -= 1
 
 print(bull)
 print(cow)
-
-
-# 9 тест не проходит
