b2bc3b2d08ddcc477ca6ea17f381c7de313254f0
Darya Filimonova <91047620+DaryaFilimonova7@users.noreply.github.com>
2023-05-28 15:27:13 +0300

    Update ex1_C.py

--- ex1_C.py
+++ ex1_C.py
@@ -1,15 +1,18 @@
 ans = [int(n1) for n1 in str(input())]
 guess = [int(n2) for n2 in str(input())]
 
-bull_cow = [0, 0]
-for i, j in zip(ans, guess):
-    if j in ans:
-        if j == i:
-            bull_cow[0] += 1
-        else:
-            if j not in guess[:j]:
-                bull_cow[1] += 1
-print(bull_cow[0])
-print(bull_cow[1])
+bull = 0
+cow = 0
 
-# 5 тест не проходит
+for i in range(len(ans)):
+        if guess[i] == ans[i]:
+            bull += 1
+        if ans[i] in guess:
+            cow += 1
+cow = cow - bull
+
+print(bull)
+print(cow)
+
+
+# 9 тест не проходит
