843bae914618d015e0dfc25a633937acda021496
Darya Filimonova <91047620+DaryaFilimonova7@users.noreply.github.com>
2023-05-29 20:22:41 +0300

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -2,14 +2,16 @@ n = int(input())
 
 word = [str(x) for x in input()]
 
-def search(word):
+def s(word):
+    begin = word
     i = 0
     while i < len(word):
         if i+1 < len(word):
             if word[i] > word[i + 1]:
-                word = word[i+1:]
+                word = word[i + 1:]
+                i = -1
         i += 1
     return(word)
 
-ans = ''.join(search(word))
+ans = ''.join(s(word))
 print(ans)
